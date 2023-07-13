41dc75fb0085220753f3396a6e49df4437a8716b
Taras Kozodoi <acateely@gmail.com>
2023-07-13 22:24:39 +0300

    fix isLoading in Button element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import Button from "@/components/ui/Button";
-import { db } from "@/lib/db";
 
 export default async function Home() {
-  await db.set("hello", "hello");
-
-  return <Button isLoading={false}>Click</Button>;
+  return (
+    <Button variant="ghost" size="lg">
+      Click
+    </Button>
+  );
 }
