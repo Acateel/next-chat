b9a8c8c68be9fbdbe9c9ff44f2ae01bad6fa4a33
Taras Kozodoi <acateely@gmail.com>
2023-07-17 20:14:27 +0300

    add onClick handlers for accept and deny friend

--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -19,6 +19,38 @@ const FriendRequests: FC<FriendRequestsProps> = ({
     incomingFriendRequests
   );
 
+  /**
+   * Accept friend request
+   * @param senderId friends id
+   */
+  const acceptFriend = async (senderId: string) => {
+    await axios.post("/api/friends/accept", {
+      id: senderId,
+    });
+
+    setFriendRequests((prev) =>
+      prev.filter((request) => request.senderId !== senderId)
+    );
+
+    router.refresh();
+  };
+
+  /**
+   * Deny friend request
+   * @param senderId friends id
+   */
+  const denyFriend = async (senderId: string) => {
+    await axios.post("/api/friends/deny", {
+      id: senderId,
+    });
+
+    setFriendRequests((prev) =>
+      prev.filter((request) => request.senderId !== senderId)
+    );
+
+    router.refresh();
+  };
+
   return (
     <>
       {friendRequests.length === 0 ? (
@@ -29,12 +61,14 @@ const FriendRequests: FC<FriendRequestsProps> = ({
             <UserPlus className="text-black" />
             <p className="font-medium text-lg">{request.senderEmail}</p>
             <button
+              onClick={() => acceptFriend(request.senderId)}
               aria-label="accept friend"
               className="w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md"
             >
               <Check className="font-semibold text-white w-3/4 h-3/4" />
             </button>
             <button
+              onClick={() => denyFriend(request.senderId)}
               aria-label="deny friend"
               className="w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md"
             >
