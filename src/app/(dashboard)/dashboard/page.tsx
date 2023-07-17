cc92aec98b9949fa562f80819739cd599a3eb608
Taras Kozodoi <acateely@gmail.com>
2023-07-17 13:14:36 +0300

    change dashoard page

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,10 +1,5 @@
-import { authOptions } from "@/lib/auth";
-import { getServerSession } from "next-auth";
-
 const Page = async ({}) => {
-  const sesstion = await getServerSession(authOptions);
-
-  return <pre>{JSON.stringify(sesstion)}</pre>;
+  return <pre>Dashboard</pre>;
 };
 
 export default Page;
