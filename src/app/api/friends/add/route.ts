6c213faa247fa430b0227289aa1740b00c17909f
Taras Kozodoi <acateely@gmail.com>
2023-07-21 14:04:42 +0300

    fix pusher triggers

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -60,7 +60,7 @@ export async function POST(req: Request) {
     //valid request, send friend request
 
     // push notification for user that we want add
-    pusherServer.trigger(
+    await pusherServer.trigger(
       toPusherKey(`user:${idToAdd}:incoming_friend_requests`),
       "incoming_friend_requests",
       {
