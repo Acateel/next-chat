31b2f8f089073f746099d1905bf8f6728370624b
Taras Kozodoi <acateely@gmail.com>
2023-07-22 19:25:53 +0300

    add dark mode in dashboard navbar

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -45,7 +45,7 @@ const Layout = async ({ children }: LayoutProps) => {
   ).length
 
   return (
-    <div className="w-full flex h-screen">
+    <div className="w-full flex h-screen dark:bg-slate-800">
       <div className="md:hidden">
         <MobileChatLayout
           friends={friends}
@@ -54,12 +54,12 @@ const Layout = async ({ children }: LayoutProps) => {
           unseenRequestCount={unseenRequestCount}
         />
       </div>
-      <div className="hidden md:flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white dark:bg-gray-700 dark:border-gray-900 px-6">
+      <div className="hidden md:flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white dark:bg-slate-700 dark:border-slate-800 px-6">
         <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
-          <Icons.Logo className="h-8 w-auto text-indigo-600" />
+          <Icons.Logo className="h-8 w-auto text-indigo-400" />
         </Link>
         {friends.length > 0 ? (
-          <div className="text-xs font-semibold leading-6 text-gray-400">
+          <div className="text-xs font-semibold leading-6 text-gray-400 dark:text-gray-100">
             Your chats
           </div>
         ) : null}
@@ -71,7 +71,7 @@ const Layout = async ({ children }: LayoutProps) => {
             </li>
             {/* Chats functions */}
             <li>
-              <div className="text-xs font-semibold leading-6 text-gray-400">
+              <div className="text-xs font-semibold leading-6 text-gray-400 dark:text-gray-100">
                 Overview
               </div>
 
@@ -82,9 +82,9 @@ const Layout = async ({ children }: LayoutProps) => {
                     <li key={option.id}>
                       <Link
                         href={option.href}
-                        className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                        className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 dark:text-zinc-50 dark:hover:bg-slate-600 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold"
                       >
-                        <span className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
+                        <span className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 dark:text-indigo-800 dark:group-hover:text-indigo-800 flex h-6 w-6 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
                           <OptionIcon className="h-4 w-4" />
                         </span>
                         <span className="truncate">{option.name}</span>
@@ -106,9 +106,9 @@ const Layout = async ({ children }: LayoutProps) => {
               <ToogleLightModeButton />
             </li>
             {/* User profile information */}
-            <li className="-mx-6 mt-auto flex items-center">
-              <div className="flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900">
-                <div className="relative h-8 w-8 bg-gray-50">
+            <li className="-mx-6 mt-auto flex items-center overflow-hidden">
+              <div className="flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900 dark:text-gray-200">
+                <div className="relative h-8 w-8 bg-gray-50 dark:bg-inherit">
                   <Image
                     fill
                     referrerPolicy="no-referrer"
@@ -120,8 +120,13 @@ const Layout = async ({ children }: LayoutProps) => {
 
                 <span className="sr-only">Your profile</span>
                 <div className="flex flex-col">
-                  <span aria-hidden="true">{session.user.name}</span>
-                  <span className="text-xs text-zinc-400" aria-hidden="true">
+                  <span aria-hidden="true" className="truncate">
+                    {session.user.name}
+                  </span>
+                  <span
+                    className="text-xs text-zinc-400 dark:text-zinc-300"
+                    aria-hidden="true"
+                  >
                     {session.user.email}
                   </span>
                 </div>
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,36 +1,37 @@
-import { cva, VariantProps } from "class-variance-authority";
-import { ButtonHTMLAttributes, FC } from "react";
-import { Loader2 } from "lucide-react";
-import { cn } from "@/lib/utils";
+import { cva, VariantProps } from 'class-variance-authority'
+import { ButtonHTMLAttributes, FC } from 'react'
+import { Loader2 } from 'lucide-react'
+import { cn } from '@/lib/utils'
 
 /**
  * Style variants for button
  */
 export const buttonVariants = cva(
-  "active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 desabled:opacity-50 disabled:pointer-events-none",
+  'active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 desabled:opacity-50 disabled:pointer-events-none',
   {
     variants: {
       variant: {
-        default: "bg-slate-900 text-white hover:db-slate-800",
-        ghost: "bg-transparent hover:text-slate-900 hover:bg-slate-200",
+        default: 'bg-slate-900 text-white hover:db-slate-800',
+        ghost:
+          'bg-transparent hover:text-slate-900 hover:bg-slate-200 dark:hover:bg-slate-600 dark:hover:text-slate-100',
       },
       size: {
-        default: "h-10 py-2 px-4",
-        sm: "h-9 px-2",
-        lg: "h-11 px-8",
+        default: 'h-10 py-2 px-4',
+        sm: 'h-9 px-2',
+        lg: 'h-11 px-8',
       },
     },
     defaultVariants: {
-      variant: "default",
-      size: "default",
+      variant: 'default',
+      size: 'default',
     },
   }
-);
+)
 
 export interface ButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  isLoading?: boolean;
+  isLoading?: boolean
 }
 
 const Button: FC<ButtonProps> = ({
@@ -50,7 +51,7 @@ const Button: FC<ButtonProps> = ({
       {isLoading ? <Loader2 className="mr-2 h-5 w-4 animate-spin" /> : null}
       {children}
     </button>
-  );
-};
+  )
+}
 
-export default Button;
+export default Button
