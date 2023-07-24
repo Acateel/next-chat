380a0262db071ce9f755456ab5fe8a034eb3cb51
Taras Kozodoi <acateely@gmail.com>
2023-07-24 17:03:53 +0300

    add dark mode in mobile nav bar

--- src/components/MobileChatLayout.tsx
+++ src/components/MobileChatLayout.tsx
@@ -1,24 +1,25 @@
-"use client";
+'use client'
 
-import { Dialog, Transition } from "@headlessui/react";
-import { Menu, X } from "lucide-react";
-import Link from "next/link";
-import { FC, Fragment, useEffect, useState } from "react";
-import Button, { buttonVariants } from "./ui/Button";
-import { Icons } from "./Icons";
-import SidebarChatList from "./SidebarChatList";
-import { Session } from "next-auth";
-import Image from "next/image";
-import { SidebarOption } from "@/types/typings";
-import FriendRequestsSidebarOption from "./FriendRequestsSidebarOption";
-import SignOutButton from "./SignOutButton";
-import { usePathname } from "next/navigation";
+import { Dialog, Transition } from '@headlessui/react'
+import { Menu, X } from 'lucide-react'
+import Link from 'next/link'
+import { FC, Fragment, useEffect, useState } from 'react'
+import Button, { buttonVariants } from './ui/Button'
+import { Icons } from './Icons'
+import SidebarChatList from './SidebarChatList'
+import { Session } from 'next-auth'
+import Image from 'next/image'
+import { SidebarOption } from '@/types/typings'
+import FriendRequestsSidebarOption from './FriendRequestsSidebarOption'
+import SignOutButton from './SignOutButton'
+import { usePathname } from 'next/navigation'
+import ToogleLightModeButton from './ToogleLightModeButton'
 
 interface MobileChatLayoutProps {
-  friends: User[];
-  session: Session;
-  sidebarOptions: SidebarOption[];
-  unseenRequestCount: number;
+  friends: User[]
+  session: Session
+  sidebarOptions: SidebarOption[]
+  unseenRequestCount: number
 }
 
 const MobileChatLayout: FC<MobileChatLayoutProps> = ({
@@ -27,23 +28,26 @@ const MobileChatLayout: FC<MobileChatLayoutProps> = ({
   sidebarOptions,
   unseenRequestCount,
 }) => {
-  const [open, setOpen] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false)
 
-  const pathname = usePathname();
+  const pathname = usePathname()
   useEffect(() => {
-    setOpen(false);
-  }, [pathname]);
+    setOpen(false)
+  }, [pathname])
 
   return (
-    <div className="fixed bg-zinc-50 border-b border-zinc-200 top-0 inset-x-0 py-2 px-4">
+    <div className="fixed bg-zinc-50 border-b border-zinc-200 dark:bg-slate-700 dark:border-slate-800 top-0 inset-x-0 py-2 px-4">
       <div className="w-full flex justify-between items-center">
         <Link
           href="/dashboard"
-          className={buttonVariants({ variant: "ghost" })}
+          className={buttonVariants({ variant: 'ghost' })}
         >
           <Icons.Logo className="h-6 w-auto text-indigo-600" />
         </Link>
-        <Button onClick={() => setOpen(true)} className="gap-4">
+        <Button
+          onClick={() => setOpen(true)}
+          className="gap-4 dark:bg-slate-600 dark:hover:bg-slate-500"
+        >
           Menu <Menu className="h-6 w-6" />
         </Button>
       </div>
@@ -64,10 +68,10 @@ const MobileChatLayout: FC<MobileChatLayoutProps> = ({
                   leaveTo="-translate-x-full"
                 >
                   <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
-                    <div className="flex h-full flex-col overflow-hidden bg-white py-6 shadow-xl">
+                    <div className="flex h-full flex-col overflow-hidden bg-white py-6 shadow-xl dark:bg-slate-700">
                       <div className="px-4 sm:px-6">
                         <div className="flex items-start justify-between">
-                          <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
+                          <Dialog.Title className="text-base font-semibold leading-6 text-gray-900 dark:text-gray-50">
                             Dashboard
                           </Dialog.Title>
                           <div className="ml-3 flex h-7 items-center">
@@ -86,7 +90,7 @@ const MobileChatLayout: FC<MobileChatLayoutProps> = ({
                         {/* Content */}
 
                         {friends.length > 0 ? (
-                          <div className="text-xs font-semibold leading-6 text-gray-400">
+                          <div className="text-xs font-semibold leading-6 text-gray-400 dark:text-gray-100">
                             Your chats
                           </div>
                         ) : null}
@@ -104,19 +108,19 @@ const MobileChatLayout: FC<MobileChatLayoutProps> = ({
                             </li>
 
                             <li>
-                              <div className="text-xs font-semibold leading-6 text-gray-400">
+                              <div className="text-xs font-semibold leading-6 text-gray-400 dark:text-gray-100">
                                 Overview
                               </div>
                               <ul role="list" className="-mx-2 mt-2 space-y-1">
                                 {sidebarOptions.map((option) => {
-                                  const Icon = Icons[option.Icon];
+                                  const Icon = Icons[option.Icon]
                                   return (
                                     <li key={option.name}>
                                       <Link
                                         href={option.href}
-                                        className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                                        className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 dark:text-zinc-50 dark:hover:bg-slate-600 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                                       >
-                                        <span className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
+                                        <span className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 dark:text-indigo-800 dark:group-hover:text-indigo-800 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
                                           <Icon className="h-4 w-4" />
                                         </span>
                                         <span className="truncate">
@@ -124,7 +128,7 @@ const MobileChatLayout: FC<MobileChatLayoutProps> = ({
                                         </span>
                                       </Link>
                                     </li>
-                                  );
+                                  )
                                 })}
 
                                 <li>
@@ -138,14 +142,21 @@ const MobileChatLayout: FC<MobileChatLayoutProps> = ({
                               </ul>
                             </li>
 
+                            {/* ToogleLightMode */}
+                            <li className="relative mt-8">
+                              <div className="absolute bottom-0 left-0">
+                                <ToogleLightModeButton />
+                              </div>
+                            </li>
+
                             <li className="-ml-6 mt-auto flex items-center">
-                              <div className="flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900">
-                                <div className="relative h-8 w-8 bg-gray-50">
+                              <div className="flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900 dark:text-gray-200">
+                                <div className="relative h-8 w-8 bg-gray-50 dark:bg-inherit">
                                   <Image
                                     fill
                                     referrerPolicy="no-referrer"
                                     className="rounded-full"
-                                    src={session.user.image || ""}
+                                    src={session.user.image || ''}
                                     alt="Your profile picture"
                                   />
                                 </div>
@@ -156,7 +167,7 @@ const MobileChatLayout: FC<MobileChatLayoutProps> = ({
                                     {session.user.name}
                                   </span>
                                   <span
-                                    className="text-xs text-zinc-400"
+                                    className="text-xs text-zinc-400 dark:text-zinc-300"
                                     aria-hidden="true"
                                   >
                                     {session.user.email}
@@ -180,7 +191,7 @@ const MobileChatLayout: FC<MobileChatLayoutProps> = ({
         </Dialog>
       </Transition.Root>
     </div>
-  );
-};
+  )
+}
 
-export default MobileChatLayout;
+export default MobileChatLayout
