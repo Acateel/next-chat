1ef93884fb518e8c5d434fce9ac6c6321fccfd0f
Taras Kozodoi <acateely@gmail.com>
2023-08-07 17:41:56 +0300

    merge auth's and locale's middlewares

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,18 @@
+import { getToken } from 'next-auth/jwt'
+import { withAuth } from 'next-auth/middleware'
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextRequest } from 'next/server'
 
 import { i18n } from '@/lib/locale/i18n-config'
 
 import { match as matchLocale } from '@formatjs/intl-localematcher'
 import Negotiator from 'negotiator'
 
+/**
+ * Get user locale
+ * @param request
+ * @returns locale in string type
+ */
 function getLocale(request: NextRequest): string | undefined {
   // Negotiator expects plain object so we need to transform headers
   const negotiatorHeaders: Record<string, string> = {}
@@ -24,9 +31,13 @@ function getLocale(request: NextRequest): string | undefined {
   return locale
 }
 
-export function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname
-
+/**
+ * add subpath with locale in response url
+ * @param request NextRequest from middleware
+ * @param pathname string pathname
+ * @returns next response with locale's subpath
+ */
+export function localeMiddleware(request: NextRequest, pathname: string) {
   // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
   // // If you have one
   // if (
@@ -56,8 +67,57 @@ export function middleware(request: NextRequest) {
       )
     )
   }
+
+  return NextResponse.next()
 }
 
+/**
+ * Middleware with auth
+ */
+export default withAuth(
+  async function middleware(req) {
+    const pathname = req.nextUrl.pathname
+
+    // Manage route protection
+    const isAuth = await getToken({ req })
+    const isLoginPage = pathname.includes('/login')
+
+    const sensitiveRoutes = ['/dashboard']
+    const isAccessingSensitiveRoute = sensitiveRoutes.some((route) =>
+      pathname.includes(route)
+    )
+
+    if (isLoginPage) {
+      if (isAuth) {
+        return localeMiddleware(req, '/dashboard')
+      }
+
+      return localeMiddleware(req, pathname)
+    }
+
+    if (!isAuth && isAccessingSensitiveRoute) {
+      return localeMiddleware(req, '/login')
+    }
+
+    if (pathname === '/') {
+      return localeMiddleware(req, '/dashboard')
+    }
+
+    return localeMiddleware(req, pathname)
+  },
+  {
+    // callbacks for call widleware every time
+    callbacks: {
+      async authorized() {
+        return true
+      },
+    },
+  }
+)
+
+/**
+ * With what pages middleware will work
+ */
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
